29c8ba3d9085a0482540734c5e3c75633bc0a83d
Gokul Krishnan <gokul@Gokuls-MacBook-Pro.local>
2024-01-12 10:14:52 +1300

    exposed ports

--- official-images/powershell-scripts/alpine.ps1
+++ official-images/powershell-scripts/alpine.ps1
@@ -1,2 +1,2 @@
-docker run --rm -d -v $PWD/data:/var/data --name=container alpine:latest /bin/sh -c "apk add --no-check-certificate bash && tail -f /dev/null"
+docker run --rm -d -v $PWD/data:/var/data -p 80:80 -p 443:443 --name=container alpine:latest /bin/sh -c "apk add --no-check-certificate bash && tail -f /dev/null"
 
--- official-images/ubuntu.sh
+++ official-images/ubuntu.sh
@@ -1,1 +1,1 @@
-docker run --rm -d -v $PWD/data:/var/data --name=container ubuntu:latest tail -f /dev/null
+docker run --rm -d -v $PWD/data:/var/data -p 80:80 -p 443:443 --name=container ubuntu:latest tail -f /dev/null
